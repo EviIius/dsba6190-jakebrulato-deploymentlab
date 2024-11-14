39a055e6796afb84ced8366f3b116e210a23e997
Jake Brulato <jakebrulato@gmail.com>
2024-11-13 19:57:19 -0500

    dfghj

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "azurerm_storage_account" "storage" {
 // Network Rules
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-dsba6190-jbrulato"
+  name                = "vnet-dsba6190-jake"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
