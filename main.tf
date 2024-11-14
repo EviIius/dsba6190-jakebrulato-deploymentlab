93962788c9265e10aeab67f3e0a24d13009b0baa
Jake Brulato <jakebrulato@gmail.com>
2024-11-13 19:54:12 -0500

    fgh

--- main.tf
+++ main.tf
@@ -42,3 +42,41 @@ resource "azurerm_storage_account" "storage" {
 
   tags = local.tags
 }
+
+
+// Network Rules
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-dsba6190-jbrulato"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                = "storageaccountname"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = "true"
+
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
+  }
+
+  tags = {
+    environment = "staging"
+  }
+}
