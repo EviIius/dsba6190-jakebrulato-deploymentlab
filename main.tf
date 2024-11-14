09cb3ac65c361987e91d978be57c3a7310cc70c0
Jake Brulato <jakebrulato@gmail.com>
2024-11-13 20:28:24 -0500

    dfghjk

--- main.tf
+++ main.tf
@@ -69,3 +69,44 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.2.0/24"]
   service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
 }
+
+// SQL Server
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_mssql_server" "ms" {
+  name                         = "jake-sqlserver"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_database" "db" {
+  name         = "sql-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result"
+  server_id    = azurerm_mssql_server.ms.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
+
+  tags = {
+    foo = "bar"
+  }
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
+  }
+
+  resource "azurerm_mssql_virtual_network_rule" "vnetrule" {
+  name      = "sql-vnet-rule"
+  server_id = azurerm_mssql_server.ms.id
+  subnet_id = azurerm_subnet.subnet.id
+}
+}
